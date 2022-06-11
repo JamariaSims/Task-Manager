b5311d4beee8d7ae7793a0647a58a2b900a10860
JamariaSims <JamariaxSims@gmail.com>
2022-06-11 16:47:37 -0400

    Side View Created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import CheckCircleOutlineOutlinedIcon from "@mui/icons-material/CheckCircleOutlineOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import EqualizerOutlinedIcon from "@mui/icons-material/EqualizerOutlined";
+import CrisisAlertOutlinedIcon from "@mui/icons-material/CrisisAlertOutlined";
+import PersonAddOutlinedIcon from "@mui/icons-material/PersonAddOutlined";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="Side_View">
+                <div className="Side_View_Header">
+                    <h1 id="Heading_Name">Task Manager</h1>
+                </div>
+                <div className="Side_View_Nav">
+                    <HomeOutlinedIcon id="SVG" />
+                    <a>Home</a>
+                    <CheckCircleOutlineOutlinedIcon id="SVG" />
+                    <a>My Tasks</a>
+                    <NotificationsNoneOutlinedIcon id="SVG" />
+                    <a>Inbox</a>
+                </div>
+                <p id="Title-Reporting">Reporting</p>
+                <div className="Reporting_Container">
+                    <EqualizerOutlinedIcon id="SVG" />
+                    <a>Portfolios</a>
+                    <CrisisAlertOutlinedIcon id="SVG" />
+                    <a>Goals</a>
+                </div>
+                <p id="Title-Favorites">Favorites</p>
+                <div className="Favorites_Container"></div>
+
+                <button id="Button-Invite">
+                    <PersonAddOutlinedIcon id="SVG" />
+                    <p>Invite Teammates</p>
+                </button>
+            </div>
+            <div className="Main_View"></div>
+        </div>
+    );
 }
 
 export default App;
