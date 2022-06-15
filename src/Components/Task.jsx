a7b19b4faf4ecb88468939f144ac81a581084782
JamariaSims <JamariaxSims@gmail.com>
2022-06-15 17:00:24 -0400

    Finshed Form State

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 function Task(props) {
-  const { task } = props;
+    const { task } = props;
 
-  console.log(props);
-  return (
-    <div>
-      <h1>Task</h1>
-      <p>{task.taskName}</p>
-    </div>
-  );
+    console.log(props);
+    return (
+        <div>
+            <h1>Task</h1>
+            <p>{task.taskName}</p>
+            <p>{task.taskDescription}</p>
+            <p>{task.taskDeadline}</p>
+            <p>{task.taskPriority}</p>
+        </div>
+    );
 }
 
 export default Task;
--- src/Views/Form.jsx
+++ src/Views/Form.jsx
@@ -1,109 +1,121 @@
 import React, { useState } from "react";
 
 const initialState = {
-  taskName: "",
-  taskDescription: "",
-  taskDeadline: "",
-  taskPriority: "",
+    taskName: "",
+    taskDescription: "",
+    taskDeadline: "",
+    taskPriority: "Normal",
 };
 function Form(props) {
-  const { tasks, setTasks } = props;
-  const [state, setState] = useState(initialState);
+    const { tasks, setTasks } = props;
+    const [state, setState] = useState(initialState);
 
-  const onInputChange = (event) => {
-    event.preventDefault();
-    const { target } = event;
-    const { name, value } = target;
-    setState({ ...state, [name]: value });
-  };
-  const onInputAdd = (event) => {
-    event.preventDefault();
-    setTasks([...tasks, state]);
-    setState(initialState);
-  };
-  return (
-    <form className="Form_Container">
-      <label>Task Name:</label>
-      <input
-        type={"text"}
-        value={state.taskName}
-        placeholder="Name"
-        onChange={onInputChange}
-        name="taskName"
-      />
-      <label>Task Description:</label>
-      <input
-        type={"text"}
-        value={state.taskDescription}
-        placeholder="Description"
-        onChange={onInputChange}
-        name="taskDescription"
-      />
-      <label>Deadline</label>
-      <input
-        type={"date"}
-        value={state.taskDeadline}
-        onChange={onInputChange}
-        name="taskDeadline"
-      />
-      <label>Priority</label>
-      <div className="Form_Priority">
-        <label id="Priority-Low">
-          Low{" "}
-          <input
-            type={"radio"}
-            value={state.taskPriority}
-            for="Priority-Low"
-            onChange={onInputChange}
-            name="taskPriority"
-          />
-        </label>
+    const onInputChange = (event) => {
+        const { target } = event;
+        const { name, value } = target;
+        setState({ ...state, [name]: value });
+        console.log(state);
+    };
+    const onInputSubmit = (event) => {
+        event.preventDefault();
+        setTasks([...tasks, state]);
+        setState(initialState);
+    };
+    const onInputReset = () => {
+        setState(initialState);
+    };
+    return (
+        <form className="Form_Container" onSubmit={onInputSubmit}>
+            <label>Task Name:</label>
+            <input
+                type={"text"}
+                value={state.taskName}
+                placeholder="Name"
+                onChange={onInputChange}
+                name="taskName"
+                required
+            />
+            <label>Task Description:</label>
+            <input
+                type={"text"}
+                value={state.taskDescription}
+                placeholder="Description"
+                onChange={onInputChange}
+                name="taskDescription"
+                required
+            />
+            <label>Deadline</label>
+            <input
+                type={"date"}
+                value={state.taskDeadline}
+                onChange={onInputChange}
+                name="taskDeadline"
+                required
+            />
+            <label>Priority</label>
+            <div className="Form_Priority">
+                <label id="Priority-Low" for="Priority-Low">
+                    Low{" "}
+                    <input
+                        id="Priority-Low"
+                        type={"radio"}
+                        value={"Low"}
+                        onChange={onInputChange}
+                        name="taskPriority"
+                        checked={state.taskPriority === "Low"}
+                    />
+                </label>
 
-        <label id="Priority-Normal">
-          Normal{" "}
-          <input
-            type={"radio"}
-            value={state.taskPriority}
-            for="Priority-Normal"
-            onChange={onInputChange}
-            name="taskPriority"
-          />
-        </label>
+                <label id="Priority-Normal" for="Priority-Normal">
+                    Normal{" "}
+                    <input
+                        id="Priority-Normal"
+                        type={"radio"}
+                        value={"Normal"}
+                        onChange={onInputChange}
+                        name="taskPriority"
+                        checked={state.taskPriority === "Normal"}
+                    />
+                </label>
 
-        <label id="Priority-Important">
-          Important{" "}
-          <input
-            type={"radio"}
-            value={state.taskPriority}
-            for="Priority-Important"
-            onChange={onInputChange}
-            name="taskPriority"
-          />
-        </label>
+                <label id="Priority-Important" for="Priority-Important">
+                    Important{" "}
+                    <input
+                        id="Priority-Important"
+                        type={"radio"}
+                        value={"Important"}
+                        onChange={onInputChange}
+                        name="taskPriority"
+                        checked={state.taskPriority === "Important"}
+                    />
+                </label>
 
-        <label id="Priority-Important">
-          Critical{" "}
-          <input
-            type={"radio"}
-            value={state.taskPriority}
-            for="Priority-Critical"
-            onChange={onInputChange}
-            name="taskPriority"
-          />
-        </label>
-      </div>
+                <label id="Priority-Important" for="Priority-Critical">
+                    Critical{" "}
+                    <input
+                        id="Priority-Critical"
+                        type={"radio"}
+                        value={"Critical"}
+                        onChange={onInputChange}
+                        name="taskPriority"
+                        checked={state.taskPriority === "Critical"}
+                    />
+                </label>
+            </div>
 
-      <label>Members</label>
-      <button>Add Member</button>
-      <label>Group</label>
-      <select>
-        <option>None</option>
-        <option>Add a group</option>
-      </select>
-      <button onClick={onInputAdd}>Add</button>
-      <button>Cancel</button>
-    </form>
-  );
+            <label>Members</label>
+            <button>Add Member</button>
+            <label>Group</label>
+            <select>
+                <option>None</option>
+                <option>Add a group</option>
+            </select>
+            <button type="submit">Add</button>
+            <button type="reset" onClick={onInputReset}>
+                Cancel
+            </button>
+        </form>
+    );
 }
 
 export default Form;
