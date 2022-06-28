304c3debca0696942c2f659906185c07d3de9acc
JamariaSims <JamariaxSims@gmail.com>
2022-06-28 16:47:31 -0400

    Added database

--- data/seeds/01-data.js
+++
@@ -1,7 +0,0 @@
-exports.seed = async function (knex) {
-    await knex("Users").truncate();
-    await knex("Passwords").truncate();
-    await knex("Tasks").truncate();
-    await knex("Projects").truncate();
-    await knex("Groups").truncate();
-};
